25b3daed2efa5e25a397cc6f17e9b0689e4e2df9
lisandrofernando <lisandrusfernandus@gmail.com>
2021-12-01 16:56:32 -0600

    first commit

---
+++ src/main/resources/com_comscope.resources/base.java
@@ -0,0 +1,48 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class base {
+	
+	
+	public static WebDriver driver;
+	public Properties prop;
+	
+	public WebDriver initializeDriver() {
+		
+prop = new Properties();
+        
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//com_professional//sample_project//data.properties");
+        prop.load(file);
+        
+        String browser_Name = prop.getProperty("browser");
+        System.out.println(browser_Name);
+        
+        if(browser_Name.equals("chrome")) {
+        	System.setProperty("webdriver.chrome.driver", "/Applications/WEbDrivers/chromedriver");
+        	driver = new ChromeDriver();
+        	
+        }
+        else if(browser_Name.equals("firefox")) {
+        	System.setProperty("webdriver.gecko.driver", "");
+        	driver = new FirefoxDriver();
+        }
+        else if(browser_Name.equals("internetexplorer")) {
+        	System.setProperty("webdriver.ie.driver", "");
+        	driver = new InternetExplorerDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		
+		
+		return driver;
+	}
+	
+
+}
---
+++ src/test/java/com_test/comscope/TestPrintLink.java
@@ -0,0 +1,37 @@
+package com_test.comscope;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestPrintLink {
+
+	@Test
+	public void printLinks() {
+		System.setProperty("webdriver.chrome.driver", "/Applications/WEbDrivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.commscope.com/");
+		driver.findElement(By.id("desktop-search-bar")).sendKeys("ME-7000");
+		driver.findElement(By.cssSelector("[aria-label='Search']")).click();
+		//System.out.println(driver.findElements(By.tagName("a")).size()); 
+		WebElement page = driver.findElement(By.id("facets-form"));
+		List<WebElement> myLinks = page.findElements(By.tagName("a"));
+		System.out.println("The links are :" + myLinks);
+		for (int i=0; i<myLinks.size(); i++) {
+			
+			System.out.println("The link are "+ myLinks.get(i).getText());
+		}
+		
+		
+		
+		
+		driver.quit();
+	
+	}
+}
